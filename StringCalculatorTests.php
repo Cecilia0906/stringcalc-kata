f3ff94fd350f167732a3a0cc7b14d165905bf92b
mgallego <moisesgallego@gmail.com>
2012-11-12 20:14:02 +0100

    one number second refactorize

--- StringCalculatorTests.php
+++ StringCalculatorTests.php
@@ -4,22 +4,27 @@ require_once "StringCalculator.php";
 
 class StringCalculatorTests extends PHPUnit_Framework_TestCase
 {
+    private $calculator;
+
+    public function setUp()
+    {
+        $this->calculator = new StringCalculator();
+    }
+    
     public function testAddEmptyString()
     {
-        $calculator = new StringCalculator();
         $expected = 0;
-        $actual = $calculator->add("");
+        $actual = $this->calculator->add("");
         $this->assertTrue($expected === $actual);
         $this->assertSame($expected, $actual);
     }
 
     public function testOneNumber()
     {
-        $calculator = new StringCalculator();
         $expected = 1;
-        $actual = $calculator->add("1");
+        $actual = $this->calculator->add("1");
         $this->assertEquals($expected, $actual);
-        $actual = $calculator->add("2");
+        $actual = $this->calculator->add("2");
         $this->assertEquals(2, $actual);
     }
 }

a6901c2a8b05238958c6a1caac913c780f0986d8
mgallego <moisesgallego@gmail.com>
2012-11-12 20:19:59 +0100

    one number with data provider

--- StringCalculatorTests.php
+++ StringCalculatorTests.php
@@ -6,6 +6,17 @@ class StringCalculatorTests extends PHPUnit_Framework_TestCase
 {
     private $calculator;
 
+    public function oneNumber()
+    {
+        return
+            [
+                [1, '1'],
+                [2, '2'],
+                [3, '3'],
+                [566637, '566637']
+            ];
+    }
+    
     public function setUp()
     {
         $this->calculator = new StringCalculator();
@@ -19,13 +30,14 @@ class StringCalculatorTests extends PHPUnit_Framework_TestCase
         $this->assertSame($expected, $actual);
     }
 
-    public function testOneNumber()
+    /**
+     * Test One Number
+     *
+     * @dataProvider oneNumber
+     */
+    public function testOneNumber($expected, $string)
     {
-        $expected = 1;
-        $actual = $this->calculator->add("1");
-        $this->assertEquals($expected, $actual);
-        $actual = $this->calculator->add("2");
-        $this->assertEquals(2, $actual);
+        $this->assertEquals($expected, $this->calculator->add($string));
     }
 }
 
